1ca5f5fa87fb4bacf517b3d7fa7020f1b67fd2fe
Talha Noman <talhanoman61@gmail.com>
2023-02-12 16:46:33 +0500

    Font with Font Weights

--- components/Welcome/Welcome.js
+++ components/Welcome/Welcome.js
@@ -9,7 +9,15 @@ SplashScreen.preventAutoHideAsync();
 
 const Welcome = () => {
     const [fontsLoaded] = useFonts({
-        'Montserrat': require('../../assets/fonts/Montserrat.ttf'),
+        'Montserrat-900': require('../../assets/fonts/Montserrat-Black.ttf'),
+        'Montserrat-800': require('../../assets/fonts/Montserrat-ExtraBold.ttf'),
+        'Montserrat-700': require('../../assets/fonts/Montserrat-Bold.ttf'),
+        'Montserrat-600': require('../../assets/fonts/Montserrat-SemiBold.ttf'),
+        'Montserrat-500': require('../../assets/fonts/Montserrat-Medium.ttf'),
+        'Montserrat-400': require('../../assets/fonts/Montserrat-Regular.ttf'),
+        'Montserrat-300': require('../../assets/fonts/Montserrat-Light.ttf'),
+        'Montserrat-200': require('../../assets/fonts/Montserrat-ExtraLight.ttf'),
+        'Montserrat-100': require('../../assets/fonts/Montserrat-Thin.ttf')
     });
     // Keep the splash screen visible while we fetch resources
     const onLayoutRootView = useCallback(async () => {
@@ -24,15 +32,39 @@ const Welcome = () => {
     return (
         <SafeAreaProvider>
             <SafeAreaView onLayout={onLayoutRootView}>
-                <Text style={styles.fontStyle} className='text-red-400 text-xl text-center'>Welcome</Text>
+                <Text style={styles.fontStyle900} className='text-red-400 text-xl text-center'>Welcome</Text>
             </SafeAreaView>
         </SafeAreaProvider>
     )
 }
 
 const styles = StyleSheet.create({
-    fontStyle : {
-        fontFamily : 'Montserrat'
-    }
+    fontStyle100 : {
+        fontFamily : 'Montserrat-100'
+    },
+    fontStyle200 : {
+        fontFamily : 'Montserrat-200'
+    },
+    fontStyle300 : {
+        fontFamily : 'Montserrat-300'
+    },
+    fontStyle400 : {
+        fontFamily : 'Montserrat-400'
+    },
+    fontStyle500 : {
+        fontFamily : 'Montserrat-500'        
+    },
+    fontStyle600 : {
+        fontFamily : 'Montserrat-600'
+    },
+    fontStyle700 : {
+        fontFamily : 'Montserrat-700'
+    }, 
+    fontStyle800 : {
+        fontFamily : 'Montserrat-800'
+    },
+    fontStyle900 : {
+        fontFamily : 'Montserrat-900'
+    }    
 })
 export default Welcome
