523df5da7c4bc504c90d855dfa3e7e3030032eed
Talha Noman <talhanoman61@gmail.com>
2023-07-19 00:33:16 +0500

    Customer Dashboard Updated

--- screens/Dashboard/Customer/CustomerDashboard.js
+++ screens/Dashboard/Customer/CustomerDashboard.js
@@ -1,31 +1,39 @@
-import { View, ScrollView, Pressable, Text, StyleSheet } from 'react-native'
+import { View, ScrollView, Pressable, Text, StyleSheet, TextInput } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React, { useState } from 'react'
 import NavHeader from '../../../components/Seller/NavHeader'
 import { getAuth, signOut } from '@firebase/auth'
-import { fontWeight500 } from '../../../assets/Styles/FontWeights'
+import { fontWeight500, fontWeight400 } from '../../../assets/Styles/FontWeights'
 import NavFooter from '../../../components/Customer/NavFooter'
 export default function CustomerDashboard({ navigation }) {
-  const [error, setError] = useState('')
-  // firebase  
-  const handleSignout = () => {
-    const auth = getAuth()
-    signOut(auth).then(() => {
-      navigation.navigate("Login")
-    }).catch((error) => {
-      // An error happened.            
-      setError(error)
-    });
-  }
+  const [error, setError] = useState('')  
+  const [search, setSearch] = useState('')
   return (
     <SafeAreaView>
       <View className='flex flex-col h-screen'>
         {/* Nav Header */}
         <NavHeader title={'CUSTOMER DASHBOARD'} />
         <ScrollView className='flex-grow px-4'>
-          <Pressable onPress={handleSignout} style={shadow} className='bg-red-500 px-4 py-2 rounded-md mt-auto flex flex-row items-center mb-4'>
-            <Text style={fontWeight500} className="text-white text-center text-base">CustomerDashboard</Text>
-          </Pressable>
+          <Text style={fontWeight400} className="text-gray-800 mt-4">Search</Text>
+          <TextInput
+            value={search}
+            onChangeText={setSearch}
+            style={fontWeight400}
+            keyboardType="numeric"
+            className="     form-control
+                            block
+                            py-1.5
+                            px-2
+                            text-base
+                            font-normal
+                            text-gray-700
+                            bg-white bg-clip-padding
+                            border border-solid border-gray-300
+                            rounded-lg
+                            w-full
+                            mb-4
+                           "
+          />
         </ScrollView>
         <NavFooter navigation={navigation} />
       </View>
