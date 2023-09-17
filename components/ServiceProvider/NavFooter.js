76382f2cfcb419f38fd9049b0e39f4b84299c647
Talha Noman <talhanoman61@gmail.com>
2023-09-17 22:32:18 +0500

    Service Providers Dashboards Updated

---
+++ components/ServiceProvider/NavFooter.js
@@ -0,0 +1,48 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react'
+import { fontWeight500 } from '../../assets/Styles/FontWeights';
+import { useRoute } from '@react-navigation/native';
+
+export default function NavFooter({ navigation }) {
+    const activeTabStyle = 'flex flex-col items-center border-t-[3px] border-[#e8b05c]'
+    const inActiveTabStyle = 'flex flex-col items-center border-[#e8b05c]'
+    const route = useRoute();
+    return (
+        <View style={style.shadow} className='fixed bottom-0 h-[60px] px-4 border-none bg-[#f7f8f8] py-2'>
+            <View className='flex flex-row items-center'>
+                <View className='w-6/12'>
+                    <View className='flex flex-row my-auto items-center justify-around'>
+                        <Pressable onPress={() => navigation.navigate('SellerDashboard')} className={route.name === 'SellerDashboard' ? activeTabStyle : inActiveTabStyle}>
+                            <AntDesign name="home" size={30} color="#e8b05c" />
+                            <Text style={fontWeight500} className='text-[#e8b05c] text-xs'>Home</Text>
+                        </Pressable>
+                        <Pressable className={route.name === 'Chats' ? activeTabStyle : inActiveTabStyle}>
+                            <AntDesign name="message1" size={30} color="#e8b05c" />
+                            <Text style={fontWeight500} className='text-[#e8b05c] text-xs'>Chats</Text>
+                        </Pressable>
+                    </View>
+                </View>            
+                <View className='w-6/12'>
+                    <View className='flex flex-row my-auto items-center justify-around'>
+                        <Pressable onPress={() => navigation.navigate('SellerAds')} className={route.name === 'SellerAds' ? activeTabStyle : inActiveTabStyle}>
+                            <Ionicons name="heart-outline" size={30} color="#e8b05c" />
+                            <Text style={fontWeight500} className='text-[#e8b05c] text-xs'>My Ads</Text>
+                        </Pressable>
+                        <Pressable onPress={() => navigation.navigate('SellerSettings')} className={route.name === 'SellerSettings' ? activeTabStyle : inActiveTabStyle}>
+                            <Ionicons name="person-outline" size={30} color="#e8b05c" />
+                            <Text style={fontWeight500} className='text-[#e8b05c] text-xs'>Account</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    shadow: {
+        elevation: 1
+    }
+})
--- screens/Dashboard/ServiceProvider/RiderDashboard.js
+++ screens/Dashboard/ServiceProvider/RiderDashboard.js
@@ -1,13 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, ScrollView, Pressable, TextInput, Image, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
+import NavHeader from '../../../components/Customer/NavHeader'
 
-export default function RiderDashboard() {
+import NavFooter from '../../../components/ServiceProvider/NavFooter'
+import { fontWeight600 } from '../../../assets/Styles/FontWeights';
+
+export default function RidersDashboard({ navigation }) {
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>RiderDashboard</Text>
+      <View className='flex flex-col h-screen'>
+        {/* Nav Header */}
+        <NavHeader title={'RIDERS DASHBOARD'} navigation={navigation} />
+        <ScrollView className='flex-grow px-4'>
+          <View className={`flex flex-row items-center border border-gray-300 rounded-md px-4 bg-white my-5`}>
+            <Icon name="search" size={20} color="#aaa" className={`mr-4`} />
+            <TextInput
+              className={`flex-1 h-10 text-black `}
+              placeholder="Search..."
+              placeholderTextColor="#aaa"
+            />
+          </View>
+          <Pressable onPress={()=> navigation.navigate('AllButchersScreen')} style={shadow} className='w-full bg-white rounded-md p-2 flex flex-row items-center mb-3'>
+            <View className='w-24'>
+              <Image
+                source={require('../../../assets/butcher.png')}
+                className='w-20 h-20 bg-contain'
+              />
+            </View>
+            <Text style={fontWeight600} className='text-lg ml-2.5'>Bookings Request</Text>
+          </Pressable>              
+        </ScrollView>
+        
+        <NavFooter navigation={navigation} />
       </View>
+      {/* Footer */}
     </SafeAreaView>
   )
 }
+
+const { shadow } = StyleSheet.create({
+  shadow: {
+    elevation: 1
+  }
+})
